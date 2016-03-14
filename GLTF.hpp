5beef848eb381180011538b1417fa56d2a7887da
iaiacthulhu <kompakuyoumu@gmail.com>
2016-03-14 14:57:19 +0900

    Create GLTF.hpp

---
+++ GLTF.hpp
@@ -0,0 +1,24 @@
+#ifndef GLTF_H
+#define GLTF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <iterator>
+#include <memory>
+
+
+#include "./glTF.hpp"
+#include "./common.hpp"
+#include "./linear_algebra.hpp"
+
+// TODO gettext用
+//#define _(X) gettext(X)
+#define _(X) X
+#define X_(X) X
+
+
+#endif
